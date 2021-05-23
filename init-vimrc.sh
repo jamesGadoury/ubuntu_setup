c0b2198a3d4472d28859faf27fcd31a3c0b98ab8
James Gadoury <gadouryjames@gmail.com>
2021-05-23 16:46:49 -0400

    fix init-vimrc.sh tab lengths

--- init-vimrc.sh
+++ init-vimrc.sh
@@ -7,6 +7,6 @@ echo set smartindent >> $file
 echo set shiftround >> $file
 echo set autoindent >> $file
 echo let s:tabwidth=3 >> $file
-echo exec 'set tabstop='    .s:tabwidth >> $file
-echo exec 'set shiftwidth=' .s:tabwidth >> $file
-echo exec 'set softtabstop='.s:tabwidth >> $file
+echo "exec 'set tabstop='    .s:tabwidth" >> $file
+echo "exec 'set shiftwidth=' .s:tabwidth" >> $file
+echo "exec 'set softtabstop='.s:tabwidth" >> $file
