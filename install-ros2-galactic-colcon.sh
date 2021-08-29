0c1ec6f2194c8b85a8bbfdd1f6dda7502508865a
James Gadoury <gadouryjames@gmail.com>
2021-08-29 16:53:32 -0400

    Update ros2 script to install colcon as well

--- install-ros2-galactic-colcon.sh
+++ install-ros2-galactic-colcon.sh
@@ -25,5 +25,6 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-a
 sudo apt update
 sudo apt install ros-galactic-desktop
 
+sudo apt install python3-colcon-common-extensions
 
 
